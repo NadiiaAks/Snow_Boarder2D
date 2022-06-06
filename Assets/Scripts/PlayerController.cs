3f767a5cf6e7b8102efffce36b645ff25a0c2761
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-06-06 18:31:30 +0300

    Add Input keys for rotate player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float torqueAmount = 1f;
+
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddTorque(-torqueAmount);
+        }
+    }
+}
