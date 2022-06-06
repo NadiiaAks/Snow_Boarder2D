b38339f739bdf0b69de0ac88451c89f363bddb8a
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-06-06 19:16:28 +0300

    Add crash logic

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class CrashDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(other.tag == "Ground")
+        {
+            Debug.Log("You lose");
+        }
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
             Debug.Log("You finished");
         }
-        
     }
 }
