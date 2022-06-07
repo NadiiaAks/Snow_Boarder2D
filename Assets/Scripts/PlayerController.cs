d41bdd6aa2af94e51a9fdda582383734d82b2fb9
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-06-07 16:43:22 +0300

    Add boost speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,38 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float torqueAmount = 1f;
+    [SerializeField] private float boostSpeed = 30f;
+    [SerializeField] private float normalSpeed = 20f;
 
     Rigidbody2D rb;
+    SurfaceEffector2D effector;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        effector = FindObjectOfType<SurfaceEffector2D>();
     }
 
     void Update()
     {
+        RotatePlayer();
+        BoostSpeed();
+    }
+
+    void BoostSpeed()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            effector.speed = boostSpeed;
+        }
+        else
+        {
+            effector.speed = normalSpeed;
+        }
+
+    }
+    void RotatePlayer()
+    {
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.AddTorque(torqueAmount);
