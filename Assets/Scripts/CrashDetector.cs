2d859e335bb75246d2f2fafe98a3361a639d7510
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-06-07 11:25:11 +0300

    Add delay time for reload scene

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] private float delayTime = 2f;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Ground")
         {
-            Debug.Log("You lose");
+            Invoke("ReloadScene", delayTime);
         }
     }
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
+
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] private float delayTime = 1f;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
-            Debug.Log("You finished");
+            Invoke("ReloadScene", delayTime);
         }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
